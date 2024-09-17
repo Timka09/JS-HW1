f5e53d00ac4a8006a50818fa20e2b463a8431af2
Timka09 <tymofij.i.naumenko@gmail.com>
2024-09-17 19:05:01 +0300

    hw

---
+++ hw.js
@@ -0,0 +1,35 @@
+// Task 1
+let myAge = 15;
+console.log(myAge);
+// Task 2
+const name = "Tima";
+console.log(name);
+// Task 3
+const isStudent = true;
+console.log(isStudent);
+// Task 4
+const myString = "Нема лиха без добра, Старий бох дасть, а чорт забра! ";
+console.log(myString);
+// Task 5
+const myNumber = 5 + 10;
+console.log(myNumber);
+// Task 6
+const myNull = null;
+console.log(myNull);
+// Task 7
+const yourName = prompt("Ведіть свої імя")
+console.log(yourName)
+alert("Ну шо ты " + yourName)
+// Task 8
+const window = confirm("Підтвердіть дію!");
+if (window)
+    alert("Дякую за підтвердження!");
+else
+    alert("Дію відмінено!");
+// Task 9
+alert("Попередження!")
+const warning = confirm("Ця дія небезпечна! Підтвердіть будь-ласка дію")
+if (warning)
+    alert("Дякую за підтвердження!");
+else
+    alert("Дію відмінено!");
